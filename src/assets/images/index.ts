6bcd0fcb00f01464dcdeac6690c2e78ebf4219ff
Виктор Сиденко <Viktorsidenko0@gmail.com>
2026-05-20 18:58:05 +0700

    add more artworks

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,11 @@
 import img0 from './0.jpg'
 import img1 from './1.jpg'
+import img10 from './10.jpg'
+import img11 from './11.jpg'
+import img12 from './12.jpg'
+import img13 from './13.jpg'
+import img14 from './14.jpg'
+import img15 from './15.jpg'
 import img2 from './2.jpg'
 import img3 from './3.jpg'
 import img4 from './4.jpg'
@@ -7,6 +13,7 @@ import img5 from './5.jpg'
 import img6 from './6.jpg'
 import img7 from './7.jpg'
 import img8 from './8.jpg'
+import img9 from './9.jpg'
 
 export const images = {
   0: img0,
@@ -18,4 +25,11 @@ export const images = {
   6: img6,
   7: img7,
   8: img8,
+  9: img9,
+  10: img10,
+  11: img11,
+  12: img12,
+  13: img13,
+  14: img14,
+  15: img15,
 } as const
